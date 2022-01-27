bff589f3dc2130c0badbb92e3ae6ccadba51c606
JACHO <jachoalexander321@gmail.com>
2022-01-26 23:21:31 -0500

    modificar datos del turno

---
+++ modificar.php
@@ -0,0 +1,27 @@
+<?php
+
+$servidor="localhost";
+$usuario="veterinaria";
+$clave="animal";
+$base="veterinaria";
+$tabla="turnos";
+
+$codigo=$_POST['codigo'];
+$cedula=$_POST['cedula'];
+$nombre=$_POST['nombre'];
+$ciudad=$_POST['ciudad'];
+$animal=$_POST['animal'];
+$telefono=$_POST['telefono'];
+
+$conexion=mysqli_connect($servidor,$usuario,$clave) or die ("error en la conexión del servidor");
+$bases=mysqli_select_db($conexion,$base) or die ("error al conectarse con la base de datos");
+
+
+
+mysqli_query($conexion,"UPDATE datos_cliente SET codigo = '$codigo', cedula = '$cedula', nombre = '$nombre', 
+ciudad = '$ciudad', animal = '$animal', telefono = '$telefono' where codigo='$codigo'") or die ("error al conectar con la tabla");
+mysqli_close($conexion);
+echo("LOS DATOS SE HAN EDITADO EXITOSAMENTE");
+
+
+?>
